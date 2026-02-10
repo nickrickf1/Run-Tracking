38758697762341581275680e3a587bbb41c2b757
Riccardo <dlz.riccardo@gmail.com>
2026-02-10 15:53:42 +0100

    fixed bugs

--- apps/backend/src/server.js
+++ apps/backend/src/server.js
@@ -1,8 +1,13 @@
-require('dotenv').config();
+const path = require("path");
+
+// forza dotenv a leggere SEMPRE apps/backend/.env
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
 const app = require("./app");
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
+    console.log("JWT_SECRET loaded?", !!process.env.JWT_SECRET);
     console.log(`API running on http://localhost:${PORT}`);
-})
+});
